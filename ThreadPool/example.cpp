9975a6813e83649a7895f7b877b911a7653ac3f9
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-09 17:58:51 +0900

    Update stringstream to ostringstream(for clear feature)

--- ThreadPool/example.cpp
+++ ThreadPool/example.cpp
@@ -13,20 +13,20 @@ int main() {
 	for (int i = 0; i < 10; i++) {
 		threadPool.EncuqueJob([]() {
 			for (int i = 0; i < 10; i++) {
-				std::stringstream ss;
-				ss << "thread" << i << " id : " << std::this_thread::get_id() << " " << i << std::endl;
-				std::cout << ss.str();
+				std::ostringstream os;
+				os << "thread" << i << " id : " << std::this_thread::get_id() << " " << i << std::endl;
+				std::cout << os.str();
 				std::this_thread::sleep_for(std::chrono::duration<double>(2));
 			}
 			}
 		);
 	}
 
-	std::stringstream ss;
+	std::ostringstream os;
 	while (true) {
-		ss << "main thread id : " << std::this_thread::get_id() << " ThreadPool::getAllThreadCount() : " << threadPool.getAllThreadCount() << " ThreadPool::getRunningJobCount() : " << threadPool.getRunningJobCount() << std::endl;
-		std::cout << ss.str();
-		ss.clear();
+		os << "main thread id : " << std::this_thread::get_id() << " ThreadPool::getAllThreadCount() : " << threadPool.getAllThreadCount() << " ThreadPool::getRunningJobCount() : " << threadPool.getRunningJobCount() << std::endl;
+		std::cout << os.str();
+		os.clear();
 		std::this_thread::sleep_for(std::chrono::duration<double>(2));
 	}
 
